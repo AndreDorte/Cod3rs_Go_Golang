cc87c1c0419ac3cb4ba5055cc87e42f930203790
André Cardoso <adorte29@gmail.com>
2020-12-27 13:35:51 -0300

    Incluindo o capitulo com os fundamentos da linguagem

---
+++ fundamentos/relacionais/relacionais.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Strings:", "banana" == "banana")
+	fmt.Println("3 != 2", 3 != 2)
+	fmt.Println("3 < 2", 3 < 2)
+	fmt.Println("3 > 2", 3 > 2)
+	fmt.Println("3 <= 2", 3 <= 2)
+	fmt.Println("3 >= 2", 3 >= 2)
+
+	d1 := time.Unix(0, 0)
+	d2 := time.Unix(0, 0)
+
+	fmt.Println("Datas:", d1 == d2)
+	fmt.Println("Datas:", d1.Equal(d2))
+
+	type Pessoa struct {
+		Nome string
+	}
+
+	p1 := Pessoa{"André"}
+	p2 := Pessoa{"André"}
+
+	fmt.Println("Pessoas:", p1 == p2)
+}
